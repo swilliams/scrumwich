68f4c5e94653623fb4dad003bf0d314894170dde
Scott Williams <scott@swilliams.me>
2013-11-22 14:56:25 -0700

    Validation

--- src/scrumwich/app/controllers/entries_controller.rb
+++ src/scrumwich/app/controllers/entries_controller.rb
@@ -4,22 +4,30 @@ class EntriesController < ApplicationController
   end
 
   def new
+    @person_name = cookies[:person_name]
+    @person_email = cookies[:person_email]
     @entry = Entry.new
   end
 
   def create
     p = Person.find_by(name: params[:person_name], email: params[:person_email])
     if p.nil?
-      p = Person.create(name: params[:person_name], email: params[:person_email])
-      if !p
+      new_person = Person.new(name: params[:person_name], email: params[:person_email])
+      if !new_person.save
+        @person_errors = new_person.errors.full_messages
+        @entry = Entry.new
         render :new
         return
       end
+      p = new_person
     end
+    cookies[:person_name] = p.name
+    cookies[:person_email] = p.email
     @entry = p.entries.build entry_params
     if @entry.save
       redirect_to today_entries_path
     else
+      @entry_errors = @entry.errors.full_messages
       render :new
     end
   end
