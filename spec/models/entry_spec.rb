fe7abd1585d69027632c02f6aade0f6baecb7c2f
Scott Williams <scott@swilliams.me>
2013-11-25 13:04:24 -0700

    Added tests for the entry model

--- spec/models/entry_spec.rb
+++ spec/models/entry_spec.rb
@@ -1,7 +1,25 @@
 require 'spec_helper'
 
 describe Entry do
+  let(:yesterday) { DateTime.now - 1 }
+
+  before do
+    p = Person.create!(name: "person1", email: "person@example.com")
+    Entry.create!(yesterday: "stuff", today: "more stuff", person_id: p.id)
+    Entry.create!(yesterday: "more stuff", today: "more stuff", person_id: p.id, created_at: yesterday)
+  end
+
   describe "#for_day" do
-    it "only gets entries for the provided day"
+    it "only gets entries for the provided day" do
+      results = Entry.for_day yesterday
+      expect(results.count).to eq(1)
+    end
+  end
+
+  describe "#for_today" do
+    it "only gets entries for today" do
+      results = Entry.for_today
+      expect(results.count).to eq(1)
+    end
   end
 end
