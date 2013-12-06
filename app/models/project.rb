8d71bbb648ac3cdfccf28dc218f6ff3beeee8e12
Scott Williams <scott@swilliams.me>
2013-12-06 13:08:30 -0700

    Added project tests.

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,8 +1,13 @@
 class Project < ActiveRecord::Base
   belongs_to :owner, class_name: :Person
   has_many :entries
-  has_and_belongs_to_many :people
+  has_and_belongs_to_many :people, -> { uniq }
 
   validates :name, presence: true, length: { maximum: 40 }
   validates :owner, presence: true
+
+  def owner=(val)
+    people << val
+    super val
+  end
 end
--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,4 +1,20 @@
 require 'spec_helper'
 
 describe Project do
+  context "setup" do
+    let (:proj) { Project.new(name: "test proj") }
+    let (:owner) { Person.create!(name: "Test Guy", email: "test@example.com") }
+
+    it "When the owner is set it is also added to the people list" do
+      proj.owner = owner
+      expect(proj.people).to include(owner)
+    end
+
+    it "Doesn't allow duplicate people" do
+      person = Person.create!(name: "foo", email: "foo@example.com")
+      proj.people << person
+      proj.people << person
+      expect(proj.people.length).to eq(1)
+    end
+  end
 end
