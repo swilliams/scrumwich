43fb2df4e80d22af913e26ccf577b90ac322c2cb
Scott Williams <scott@swilliams.me>
2013-12-16 10:02:53 -0700

    Added last_10_days for project

--- app/models/project.rb
+++ app/models/project.rb
@@ -10,4 +10,12 @@ class Project < ActiveRecord::Base
     people << val
     super val
   end
+
+  def last_10_days
+    (0..9).map do |days_back|
+      date = DateTime.now - days_back
+      results = entries.for_day date
+      results.empty? ? date : results
+    end
+  end
 end
--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -17,4 +17,25 @@ describe Project do
       expect(proj.people.length).to eq(1)
     end
   end
+
+  describe "#last_10_days" do
+    let (:person) { Person.create! name: "Test User", email: "test@example.com" }
+    let (:project) { Project.create! name: "Test Project", owner: person }
+
+    it "returns 10 items regardless" do
+      result = project.last_10_days
+      expect(result.count).to eq 10
+    end
+
+    it "records an entry entry for the row if one exists at the date" do
+      project.entries.create!(yesterday: "old", today: "new", person: person, created_at: DateTime.now - 1)
+      result = project.last_10_days
+      expect(result[1].class).to eq ActiveRecord::AssociationRelation::ActiveRecord_AssociationRelation_Entry
+    end
+
+    it "records a datetime when entries do not exist for that date" do
+      result = project.last_10_days
+      expect(result[0].class).to eq DateTime
+    end
+  end
 end
